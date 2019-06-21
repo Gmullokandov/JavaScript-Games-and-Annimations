f1f030f6700f4971db2c8ab0872f886fa9ae14f6
Gabriel Mullokandov <Gabymullokandov@gmail.com>
2019-06-20 20:19:09 -0400

    Add files via upload

---
+++ Honest Clickbait/clickbait.php
@@ -0,0 +1,61 @@
+<?php
+  define("TITLE", "Honest Clickbait Headlines");
+  include("functions.php");
+
+  if(isset($_POST["fix_submit"]))
+  {
+    //Call Function
+    CheckForClickbait();
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo TITLE; ?></title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="container">
+      <h1><?php echo TITLE; ?></h1>
+      <p class="lead"> Hate clickbait? Turn those annoying headlines into realistic and honest ones using this simple program.</p>
+
+      <div class="row">
+        <form class="col-sm-8 col-offset-2" action="" method="post">
+          <textarea placeholder="Place clickbait headline here" class="form-control input-lg" name="clickbait_headline"></textarea><br>
+          <button type="submit" class="btn btn-primary btn-lg pull-right" name="fix_submit">Make Honest!</button>
+        </form>
+      </div>
+
+      <?php
+        if(isset($_POST["fix_submit"]))
+        {
+          //get first value in array returned by CheckForClickbait() function
+          //store it in a variable
+          $clickbait=CheckForClickbait()[0];
+
+          //get second value in array returned by CheckForClickbait() function
+          //store it in a variable
+          $honestHeadline=CheckForClickbait()[1];
+
+          //call function
+          //pass two arguments in the funtion
+          displayHonestHeadline($clickbait, $honestHeadline);
+        }
+      ?>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
